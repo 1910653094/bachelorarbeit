2bc4e8afede9a185254d8e2ef4a53adee8141c00
Fabian Peyrat <1910653094@stud.fh-kufstein.ac.at>
2022-02-24 23:47:19 +0100

    started styling front page

--- client-react/src/pages/Home/Home.jsx
+++ client-react/src/pages/Home/Home.jsx
@@ -5,10 +5,52 @@ import './Home.css';
 export const Home = () => {
     const navigate = useNavigate();
 
+    const skills = [
+        {
+            name: 'JavaScript',
+            rating: 8
+        },
+        {
+            name: 'CSS',
+            rating: 7
+        }
+    ];
+
     return (
         <div className='container'>
-            Hello World!
-            <button onClick={() => navigate('/contact')}>Contact Me!</button>
+            <div className='email-contact-container'>
+                Email
+                <p>fabian.peyrat01@gmail.com</p>
+                or
+                <p>contact me
+                    <span onClick={() => navigate('/contact')}>here</span>
+                </p>
+            </div>
+            <div className='container-left'></div>
+            <div className='container-right'>
+                <div className='name-container'>
+                    <h1>Fabian</h1>
+                    <h1>Peyrat</h1>
+                    <h1>Full-Stack Developer</h1>
+                </div>
+                <div className='skills-container'>
+                    <h2>Skills</h2>
+                    {
+                        skills.map(skill =>
+                            <div className='skill'>
+                                { skill.name }
+                                <div className='dots-container'>
+                                    {
+                                        [...Array(10).keys()].map(i =>
+                                            <div className={'dot ' + (i <= skill.rating ? 'fill' : '')}/>
+                                        )
+                                    }
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
         </div>
     );
 };
